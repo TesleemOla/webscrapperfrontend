63bd5d878dcb6886ef80b6d69a8432049ad63cd7
Tasleemofx <oladepotesleem5@gmail.com>
2022-03-15 18:52:00 +0100

    first commit of interface

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect} from "react"
+import axios from "axios"
+
+
+function App() {
+  const [data, setData] = useState([])
+  useEffect(()=>{
+    axios.get("http://nodewebscrapper.herokuapp.com/news")
+    .then(result=>{
+      setData(result.data)
+    })
+  },[])
+  return (
+    <div className="App">
+      {data.map(({title, url, source, id})=>{
+        return <div key={id} className="card">
+            {source === "BBC"? <img src="./images/bbc.png" alt="BBC"/>:
+            source === "Guardian"? <img src="./images/guardian.png" alt="guardian"/>:
+            <img src="./images/reuters.png" alt="reuters"/> }
+            <h2>{title}</h2>
+            <h2>
+              <a href={url} target="_blank" rel="noopenner noreferrer">
+                Read more
+              </a>
+              </h2>
+          </div>
+        
+      
+      }
+    )}
+    </div>
+  );
+}
+
+export default App;
